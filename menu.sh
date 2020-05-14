fedc7e5a76e3b6ca48f65c2159bfa07fec17f834
mflo80 <mflorio@HOUSE-SERVER.florio.com.uy>
2020-05-14 18:19:36 -0300

    Se modifica el menu

--- menu.sh
+++ menu.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-PS3="Elija una opcion:"
-OPCIONES="Ejercicio1 Ejercicio2 Ejercicio3 Ejercicio4 Ejercicio5 Ejercicio6 Salir"
-select opt in $OPCIONES; do
+
+function salir {
+   if [ $1 -eq 1 ]; then
+    echo
+    exit
+   fi
+}
+
+function menu {
+  PS3="Elija una opcion: "
+  OPCIONES="Ejercicio1 Ejercicio2 Ejercicio3 Ejercicio4 Ejercicio5 Ejercicio6 Salir"
+  select opt in $OPCIONES; do
 	if [ "$opt" = "Salir" ];
 	then
 		clear
@@ -11,20 +20,40 @@ select opt in $OPCIONES; do
 	then
 		echo Abriendo Ejercicio1
 	       	./ejercicio1.sh
+                salir $?
+                break
 	elif [ "$opt" = "Ejercicio2" ];
 	then
 		echo Abriendo Ejercicio2
 	       	./ejercicio2.sh
+                salir $?
+                break
 	elif [ "$opt" = "Ejercicio3" ];
 	then
 		echo Abriendo Ejercicio3
 		./ejercicio3.sh
+                salir $?
+                break
 	elif [ "$opt" = "Ejercicio4" ];
 	then
 		echo Abriendo Ejercicio4
 		./ejercicio4.sh
+                salir $?
+                break
+        elif [ "$opt" = "Ejercicio5" ];
+        then
+                echo Abriendo Ejercicio5
+                ./ejercicio5.sh
+                salir $?
+                break
+        elif [ "$opt" = "Ejercicio6" ];
+        then
+                echo Abriendo Ejercicio6
+                ./ejercicio6.sh -a
+                salir $?
+                break
 	else
-		echo opcion erronea
+		echo Opcion erronea
 		echo Las opciones son:
 		echo 1: Ejercicio1
 		echo 2: Ejercicio2
@@ -34,4 +63,11 @@ select opt in $OPCIONES; do
 		echo 6: Ejercicio6
 		echo 7: Salir
 	fi
+ done
+}
+
+while true; do
+   echo
+   echo "MENU PRINCIPAL"
+   menu
 done
